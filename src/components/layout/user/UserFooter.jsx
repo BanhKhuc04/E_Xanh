f32103dd89ba2eec80c61996921f5a3b3567fd65
BanhKhuc04 <vanhkhuc2k5@gmail.com>
2026-06-11 22:02:24 +0700

    Update footer sovereignty badge and logo size

--- src/components/layout/user/UserFooter.jsx
+++ src/components/layout/user/UserFooter.jsx
@@ -6,6 +6,15 @@ function UserFooter() {
   return (
     <footer className="user-footer">
       <div className="shell shell--wide user-footer__content">
+        <div className="user-footer__badge">
+          <span className="user-footer__badge-icon">
+            <svg viewBox="-100 -100 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <polygon points="0,-100 22.451,-30.902 95.106,-30.902 36.327,11.803 58.779,80.902 0,38.197 -58.779,80.902 -36.327,11.803 -95.106,-30.902 -22.451,-30.902" fill="#FFFF00"/>
+            </svg>
+          </span>
+          Hoàng Sa & Trường Sa là của Việt Nam!
+        </div>
+
         <div className="user-footer__brand">
           <BrandLogo to="/" size="footer" />
           <p>Dùng điện thông minh, sống xanh bền vững.</p>
